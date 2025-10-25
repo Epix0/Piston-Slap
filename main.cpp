d21dc66ea125f5818c04eb5ae680a8f7c557667f
Epix0 <isaacstettler@gmail.com>
2025-10-25 11:48:35 -0400

    GLFW dll and dir configs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
+// sys
 #include <iostream>
 #include <memory>
+
+// vendor
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// personal
 #include "InstanceService.h"
 #include "PhysicsObject.h"
 
 int main() {
-
+	int a;
+	std::cin >> a;
 	return 0;
 }
