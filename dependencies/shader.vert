cbaf3d87a4829d19fc6b3a5b4b6c28f7d6399199
Epix0 <isaacstettler@gmail.com>
2025-10-28 21:08:00 -0400

    assimp

--- dependencies/shader.vert
+++ dependencies/shader.vert
@@ -3,14 +3,13 @@ layout (location = 0) in vec3 aPos;
 uniform mat4 view;
 uniform mat4 projection;
 uniform mat4 model;
-uniform float yScale;
 
 out vec4 FragColor;
 void main()
 {
-	vec4 finalPos = vec4(aPos.x, aPos.y * yScale, aPos.z, 1.0f);
+	vec4 finalPos = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
 		
 	gl_Position = projection * view * model * finalPos;
 
-	FragColor = (yScale == 0.0f) ? vec4(0.0f, 0.0f, 1.0f, 1.0f) : vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 };
--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+
 // personal
 #include "InstanceService.h"
 #include "PhysicsObject.h"
@@ -17,11 +21,33 @@ static void error_callback(int error, const char* description)
 	fprintf(stderr, "Error: %s\n", description);
 }
 
+static bool DoTheImportThing(const std::string& pFile) {
+	// Create an instance of the Importer class
+	Assimp::Importer importer;
+
+	// And have it read the given file with some example postprocessing
+	// Usually - if speed is not the most important aspect for you - you'll
+	// probably to request more postprocessing than we do in this example.
+	const aiScene* scene = importer.ReadFile(pFile,
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_SortByPType);
+
+	// If the import failed, report it
+	if (nullptr == scene) {
+		//DoTheErrorLogging(importer.GetErrorString());
+		return false;
+	}
+
+	// Now we can access the file's contents.
+	//DoTheSceneProcessing(scene);
+
+	// We're done. Everything will be cleaned up by the importer destructor
+	return true;
+}
+
 int main() {
-	glm::vec3 myVec(3.0f);
-
-	std::cout << myVec.x;
-
 	GLFWwindow* window;
 	int width = 600;
 	int height = 600;
@@ -52,6 +78,8 @@ int main() {
 		glViewport(0, 0, width, height);
 		glClearColor(.2f, 0, .5f, 0);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
