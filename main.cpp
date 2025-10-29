7641da145304290d13d5b9e904b760bedf0f830d
Epix0 <isaacstettler@gmail.com>
2025-10-28 21:09:19 -0400

    cleanup main and header update

--- main.cpp
+++ main.cpp
@@ -8,10 +8,6 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
-#include <assimp/Importer.hpp>      // C++ importer interface
-#include <assimp/scene.h>           // Output data structure
-#include <assimp/postprocess.h>     // Post processing flags
-
 // personal
 #include "InstanceService.h"
 #include "PhysicsObject.h"
@@ -19,32 +15,6 @@
 static void error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Error: %s\n", description);
-}
-
-static bool DoTheImportThing(const std::string& pFile) {
-	// Create an instance of the Importer class
-	Assimp::Importer importer;
-
-	// And have it read the given file with some example postprocessing
-	// Usually - if speed is not the most important aspect for you - you'll
-	// probably to request more postprocessing than we do in this example.
-	const aiScene* scene = importer.ReadFile(pFile,
-		aiProcess_CalcTangentSpace |
-		aiProcess_Triangulate |
-		aiProcess_JoinIdenticalVertices |
-		aiProcess_SortByPType);
-
-	// If the import failed, report it
-	if (nullptr == scene) {
-		//DoTheErrorLogging(importer.GetErrorString());
-		return false;
-	}
-
-	// Now we can access the file's contents.
-	//DoTheSceneProcessing(scene);
-
-	// We're done. Everything will be cleaned up by the importer destructor
-	return true;
 }
 
 int main() {
