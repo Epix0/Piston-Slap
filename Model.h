ab4dbb0291d5fa9ea22525d3d7af1100f7a050a0
Epix0 <isaacstettler@gmail.com>
2025-11-10 17:55:21 -0500

    new members

--- Model.h
+++ Model.h
@@ -1,16 +1,35 @@
 #pragma once
 #include "Mesh.h"
-#include "vector"
-#include "string"
+#include <vector>
+#include <string>
+#include "ShaderProgram.h"
+#include <glad/glad.h>
 
-struct Model {
+class Model {
 public:
-	Model() : mNumOfMeshes(0), mName(std::string("Model")) {}
-	Model(const std::string& modelName) : mNumOfMeshes(0), mName(modelName) {}
+	friend class CustomModelImporter;
+	Model(const std::string& modelName = std::string("Unnamed_Model")) : mNumOfMeshes(0), mName(modelName) {}
+	Model(const Model&) = delete;
+
+	void Draw(ShaderProgram& shaderProgam);
+private:
+	enum BufferId {
+		VAO,
+		VBO,
+		EBO,
+		_total_ids
+	};
+
+	static GLuint mBufferIdAssignment;
 
 	// This will be the size of mMeshes during iteration
 	unsigned int mNumOfMeshes;
 	// Vector of all the Meshes that make up this model.
 	std::string mName;
 	std::vector<Mesh> mMeshes;
+	// Rendering stuffs
+	std::vector<GLuint> mBuffers;
+
+	// Returns and increments mBufferIdAssignment
+	GLuint GetBufferId();
 };
