e9db88dc8870a632b698f41949971e0d536c1d6a
Epix0 <isaacstettler@gmail.com>
2025-11-15 12:02:26 -0500

    converted direct ptr indexing to glm's handler

--- ShaderProgram.h
+++ ShaderProgram.h
@@ -25,6 +25,6 @@ public:
     void setFloat(const std::string& name, float value) const;
     void setMat4(const std::string& name, const glm::mat4& mat) const;
     void setVec3(const std::string& name, const glm::vec3& vec3) const;
-    void setVec3Array(const std::string& name, unsigned int count, const glm::vec3& vec) const;
+    void setVec3Array(const std::string& name, unsigned int count, const glm::vec3& vec3) const;
 };
 
