52d4eb15580ef998d27fbf250855687bfe990b0f
Epix0 <isaacstettler@gmail.com>
2025-11-16 18:37:40 -0500

    added support during model import
    Scene processing was originally done through the RootNote's children only, leaving behind deeply nested aiNodes. This update now processes nodes recursively, capturing the entire imported hierarchy

--- CustomIModelImporter.cpp
+++ CustomIModelImporter.cpp
@@ -4,51 +4,55 @@
 #include <assimp/postprocess.h>     // Post processing flags
 #include <iostream>
 #include <filesystem>
+#include <mutex>
 
-// TODO: need to create a function to handle child nodes OF nodes. Currently only supports single-layered nodes from the RootNode
+using std::mutex;
+
+void CustomModelImporter::ProcessNode(const aiNode* node, Model& model, const aiScene* scene) const {
+    if (node->mNumChildren > 0) {
+        for (size_t i = 0; i < node->mNumChildren; ++i) {
+            ProcessNode(node->mChildren[i], model, scene);
+        }
+    }
+
+    for (unsigned int meshIndex = 0; meshIndex < node->mNumMeshes; ++meshIndex) {
+        aiMesh* sceneMeshChild = scene->mMeshes[meshIndex];
+
+        Mesh& meshOfModel = model.mMeshes.emplace_back(sceneMeshChild->mName.C_Str());
+
+        meshOfModel.mVertices.reserve(sceneMeshChild->mNumVertices);
+        meshOfModel.mNormals.reserve(sceneMeshChild->mNumVertices);
+        meshOfModel.mElements.reserve(static_cast<size_t>(sceneMeshChild->mNumFaces) * 3);
+
+        // Elements
+        aiFace* facesArray = sceneMeshChild->mFaces;
+        for (size_t i = 0; i < sceneMeshChild->mNumFaces; ++i) {
+            auto& face = facesArray[i];
+            auto* indicesArray = face.mIndices;
+
+            for (size_t j = 0; j < face.mNumIndices; ++j) {
+                meshOfModel.mElements.push_back(indicesArray[j]);
+            }
+        }
+
+        // Vertices and Normals
+        for (unsigned int vertexIndex = 0; vertexIndex < sceneMeshChild->mNumVertices; vertexIndex++) {
+            auto& vertex = sceneMeshChild->mVertices[vertexIndex];
+            auto& normal = sceneMeshChild->mNormals[vertexIndex];
+            meshOfModel.mVertices.emplace_back(static_cast<float>(vertex.x), static_cast<float>(vertex.y), static_cast<float>(vertex.z));
+            meshOfModel.mNormals.emplace_back(static_cast<float>(normal.x), static_cast<float>(normal.y), static_cast<float>(normal.z));
+        }
+    }
+}
+
 void CustomModelImporter::ProcessScene(const aiScene* scene, std::string&& modelName) {
     Model& model = mImportedModels.emplace_back();
     model.mName = modelName;
     model.mMeshes.reserve(scene->mNumMeshes);
     model.mNumOfMeshes = scene->mNumMeshes;
 
-    /*
-    Create Model ->
-    From the rootnode, go through each node->
-    With each node, go through all its meshes ->
-        -> Through each mesh instance, create a Mesh obj and add it to the Model
-    */
-
     for (unsigned int childNodeIndex = 0; childNodeIndex < scene->mRootNode->mNumChildren; ++childNodeIndex) {
-        aiNode* node = scene->mRootNode->mChildren[childNodeIndex];
-        for (unsigned int meshIndex = 0; meshIndex < node->mNumMeshes; ++meshIndex) {
-            aiMesh* sceneMeshChild = scene->mMeshes[meshIndex];
-            
-            Mesh& meshOfModel = model.mMeshes.emplace_back(sceneMeshChild->mName.C_Str());
-
-            meshOfModel.mVertices.reserve(sceneMeshChild->mNumVertices);
-            meshOfModel.mNormals.reserve(sceneMeshChild->mNumVertices);
-            meshOfModel.mElements.reserve(static_cast<size_t>(sceneMeshChild->mNumFaces) * 3);
- 
-            // Elements
-            aiFace* facesArray = sceneMeshChild->mFaces;
-            for (size_t i = 0; i < sceneMeshChild->mNumFaces; ++i) {
-                auto& face = facesArray[i];
-                auto* indicesArray = face.mIndices;
-
-                for (size_t j = 0; j < face.mNumIndices; ++j) {
-                    meshOfModel.mElements.push_back(indicesArray[j]);
-                }
-            }
-
-            // Vertices and Normals
-            for (unsigned int vertexIndex = 0; vertexIndex < sceneMeshChild->mNumVertices; vertexIndex++) {
-                auto& vertex = sceneMeshChild->mVertices[vertexIndex];
-                auto& normal = sceneMeshChild->mNormals[vertexIndex];
-                meshOfModel.mVertices.emplace_back(static_cast<float>(vertex.x), static_cast<float>(vertex.y), static_cast<float>(vertex.z));
-                meshOfModel.mNormals.emplace_back(static_cast<float>(normal.x), static_cast<float>(normal.y), static_cast<float>(normal.y));
-            }
-        }
+        ProcessNode(scene->mRootNode->mChildren[childNodeIndex], model, scene);
     }
 }
 
@@ -60,11 +64,11 @@ bool CustomModelImporter::ImportModelFile(const std::string& pFile) {
     // Usually - if speed is not the most important aspect for you - you'll
     // probably to request more postprocessing than we do in this example.
     const aiScene* scene = importer.ReadFile(pFile,
-        aiProcess_JoinIdenticalVertices |
         aiProcess_SortByPType|
+        aiProcess_JoinIdenticalVertices |
         aiProcess_Triangulate|
-        aiProcess_FixInfacingNormals|
-        aiProcess_GenNormals);
+        aiProcess_FindDegenerates|
+        aiProcess_FixInfacingNormals);
 
     // If the import failed, report it
     if (nullptr == scene) {
--- CustomIModelImporter.h
+++ CustomIModelImporter.h
@@ -12,5 +12,10 @@ public:
 	bool ImportModelFile(const std::string& pFile);
 	std::vector<Model> mImportedModels;
 private:
+
+	// Intended to be used recursively.
+	// @param parentModel ref is where the node will shove its data
+	void ProcessNode(const aiNode* node, Model& model, const aiScene* scene) const;
+
 	void ProcessScene(const aiScene* scene, std::string&& modelName);
 };
