a73ac7348bbf118c388355a5d8ae363810050a13
Epix0 <isaacstettler@gmail.com>
2025-11-21 19:59:12 -0500

    beginning of texture support

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,6 +1,9 @@
 #include "Mesh.h"
+#include "ShaderProgram.h"
 #include <glad/glad.h>
 #include <cstddef>
+
+#include <iostream>
 
 constexpr GLuint totalVertexAttributes = 3;
 constexpr void* ptrZero = 0;
@@ -15,9 +18,9 @@ void Mesh::PrepareForGL() {
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * mVertexData.size(), mVertexData.data(), GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * mElements.size(), mElements.data(), GL_STATIC_DRAW);
 
 	// Position, Normal, TexCoords
@@ -32,7 +35,34 @@ void Mesh::PrepareForGL() {
 	glBindVertexArray(0);
 }
 
-void Mesh::Draw() const {
+void Mesh::Draw(ShaderProgram& shader) const {
+	GLuint diffuseNr = 1;
+	GLuint heightNr = 1;
+	GLuint normalNr = 1;
+	GLuint specularNr = 1;
+
+	// tex proc
+    for (GLuint i = 0; i < mTextureData.size(); i++)
+    {
+        glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
+        // retrieve texture number (the N in diffuse_textureN)
+        string number;
+        string name = mTextureData[i].type;
+        if (name == "texture_diffuse")
+            number = std::to_string(diffuseNr++);
+        else if (name == "texture_specular")
+            number = std::to_string(specularNr++); // transfer unsigned int to string
+        else if (name == "texture_normal")
+            number = std::to_string(normalNr++); // transfer unsigned int to string
+        else if (name == "texture_height")
+            number = std::to_string(heightNr++); // transfer unsigned int to string
+
+        // now set the sampler to the correct texture unit
+        glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
+        // and finally bind the texture
+        glBindTexture(GL_TEXTURE_2D, mTextureData[i].id);
+    }
+
 	glBindVertexArray(VAO);
 	glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(mElements.size()), GL_UNSIGNED_INT, ptrZero);
 }
