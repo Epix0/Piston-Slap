5554f0cc88a02521ede637a7db9c5b406f7d5e90
Epix0 <isaacstettler@gmail.com>
2025-11-28 19:05:19 -0500

    carved logic in favor of WorldObject

--- Model.cpp
+++ Model.cpp
@@ -1,41 +1,10 @@
 #include "Model.h"
 #include "ShaderProgram.h"
-#include "glm/glm.hpp"
-#include "glm/ext/matrix_transform.inl"
-#include "World.h"
 #include "Mesh.h"
 
-const glm::vec3 Unit_Vector_Pitch = glm::vec3(1.0f, 0, 0);
-const glm::vec3 Unit_Vector_Yaw = glm::vec3(0, 1.0f, 0);
-const glm::vec3 Unit_Vector_Roll = glm::vec3(0, 0, 1.0f);
-
 void Model::draw(ShaderProgram& shader) const {
-	shader.setMat4("model", mWorldTransform);
-	for (auto& mesh : mMeshes) {
+	shader.setMat4("model", getWorldTransform());
+	for(auto& mesh : mMeshes) {
 		mesh.draw(shader);
 	}
-}
-
-void Model::setPos(glm::vec3 newPos) {
-	mWorldPos = newPos;
-}
-
-void Model::pushTransformUpdate() {
-	glm::mat4 model = World::getWorld()->getWorldMatrix();
-
-	model = glm::translate(model, mWorldPos);
-	model = glm::rotate(model, mWorldOrientation.x, Unit_Vector_Pitch);
-	model = glm::rotate(model, mWorldOrientation.y, Unit_Vector_Yaw);
-	model = glm::rotate(model, mWorldOrientation.z, Unit_Vector_Roll);
-	model = glm::scale(model, mScale);
-
-	mWorldTransform = model; // set last to ensure thread safety I reckon
-}
-
-void Model::setOrientationDeg(glm::vec3 newOrientation) {
-	mWorldOrientation = glm::vec3(glm::radians(newOrientation));
-}
-
-void Model::setOrientationRad(glm::vec3 newOrientation) {
-	mWorldOrientation = newOrientation;
 }
--- Model.h
+++ Model.h
@@ -3,16 +3,14 @@
 #include "ShaderProgram.h"
 #include <string>
 #include <vector>
-#include "glad/glad.h"
-#include "glm/fwd.hpp"
+#include "WorldObject.h"
 
 using std::string, std::vector;
 
-class Model {
+class Model : public WorldObject {
 public:
 	friend class CustomModelImporter;
-	Model(const string& modelName = string("Unnamed_Model"))
-		: mName(modelName), mWorldTransform(1.0f), mWorldPos(0), mWorldOrientation(0), mScale(1.0f) {};
+	Model(const string& modelName = string("Unnamed_Model")) {};
 	
 	~Model() = default;
 
@@ -20,42 +18,11 @@ public:
 	// This function is only concerned with getting the Model on the screen and translated from World space.
 	// View, Projection and other uniforms should be set prior to this call.
 	void draw(ShaderProgram& shader) const;
-
-	void setPos(glm::vec3 newPos);
-
-	// Updates mWorldTransform of model's position, scale, and rotation.
-	// Intended to be called at the end of all transform-related updates.
-	void pushTransformUpdate();
-
-	// @newOrientation is in Euler angles DEGREES
-	// Ensure to call updateMatrix() to finalize this update
-	void setOrientationDeg(glm::vec3 newOrientation);
-
-	// @newOrientation is in Euler angles RADIANS
-	// Ensure to call updateMatrix() to finalize this update
-	void setOrientationRad(glm::vec3 newOrientation);
-
-	inline glm::vec3 getPos() const { return mWorldPos; }
-	inline glm::vec3 getOrientation() const { return mWorldOrientation; }
-	inline glm::vec3 getScale() const { return mScale; }
+		
 	inline size_t getMeshCount() const { return mMeshes.size(); }
 
-	// Technical alias, though not a friendly identifier at times
-	std::string mName;
+
 private:
-	// A matrix with the World identity comprised of Model's scale, position, and rotation.
-	// Written to when updateMatrix() is called
-	glm::mat4 mWorldTransform;
-
-	// XYZ placement relative to world
-	glm::vec3 mWorldPos;
-
-	// Euler angles in radians
-	glm::vec3 mWorldOrientation;
-	
-	// XYZ model scale
-	glm::vec3 mScale;
-
 	// Model's Meshes
 	std::vector<Mesh> mMeshes;
 };
