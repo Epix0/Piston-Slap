fa4371c2980c15ce1629383ad32d4cd6e609f7dc
Epix0 <isaacstettler@gmail.com>
2025-11-29 09:47:55 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,8 +2,12 @@
 #include "WorldObject.h"
 #include <stack>
 
+// The Player is 
+
 class Player : public WorldObject {
 public:
+	Player() : mHeight(0) {};
+
 	enum class PlayerAction {
 		None = 1,
 		Forward,
@@ -21,6 +25,8 @@ public:
 
 	// Pops an enum action from the input stack
 	PlayerAction popAction();
+
+	size_t mHeight;
 private:
 	std::stack<PlayerAction> mPlayerActionsStack;
 };
