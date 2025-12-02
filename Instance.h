8122b0588e925364c5f484d81f81ccfce8822ba2
Epix0 <isaacstettler@gmail.com>
2025-12-02 18:49:26 -0500

    mpModel to wkpmModel

--- Instance.h
+++ Instance.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Model.h"
 #include "WorldObject.h"
+#include "ShaderProgram.h"
 #include <string>
 #include <memory>
 
@@ -9,10 +10,17 @@
 
 class Instance : public WorldObject {
 public:
-	Instance(const std::string& baseName="unnamed_instance") : cmBaseName(baseName) {};
+	Instance(const std::string& baseName = "unnamed_instance", std::weak_ptr<Model> pModel = {}) : cmBaseName(baseName), mpModel(pModel) {};
+
+	inline void draw(ShaderProgram& shader) const {
+		if(auto pModel = mpModel.lock()) {
+			shader.setMat4("model", getWorldTransform());
+			pModel->draw(shader);
+		}
+	};
 private:
 	// The reference points to a target element from CustomImporter.mImportedModels
-	std::shared_ptr<Model> mpModelRef;
+	std::weak_ptr<Model> mpModel;
 
 	// The technical alias of this Instance during registration. This is also the key to use when constructing.
 	const std::string cmBaseName;
