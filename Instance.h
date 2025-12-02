7e81568b0d479156ed3e766cd4a512dd030d40be
Epix0 <isaacstettler@gmail.com>
2025-12-01 22:56:58 -0500

    Update Instance.h

--- Instance.h
+++ Instance.h
@@ -2,16 +2,17 @@
 #include "Model.h"
 #include "WorldObject.h"
 #include <string>
+#include <memory>
 
 // An Instance is a world unit that is used in the game loop. An Instance
 // will have transformation data and mesh data.
 
 class Instance : public WorldObject {
 public:
-	Instance() = default;
+	Instance(const std::string& baseName="unnamed_instance") : cmBaseName(baseName) {};
 private:
 	// The reference points to a target element from CustomImporter.mImportedModels
-	const Model& cmModelRef;
+	std::shared_ptr<Model> mpModelRef;
 
 	// The technical alias of this Instance during registration. This is also the key to use when constructing.
 	const std::string cmBaseName;
