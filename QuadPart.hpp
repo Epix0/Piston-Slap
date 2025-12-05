0480e32e724b1edd91d239d6003eb5323020b242
Epix0 <isaacstettler@gmail.com>
2025-12-05 16:11:53 -0500

    Create QuadPart.hpp

---
+++ QuadPart.hpp
@@ -0,0 +1,10 @@
+#include "Instance.h"
+#include <string>
+
+class QuadPart : public Instance {
+public:
+	QuadPart(const std::string& uniqueName = "QuadPart") : Instance(uniqueName, {}) {};
+
+private:
+
+};
