86735810958e349e1d3f752c08fc5134620cebff
Epix0 <isaacstettler@gmail.com>
2025-12-08 14:53:51 -0500

    Update QuadPart.hpp

--- QuadPart.hpp
+++ QuadPart.hpp
@@ -1,10 +1,15 @@
 #include "Instance.h"
 #include <string>
+#include <memory>
 
 class QuadPart : public Instance {
 public:
-	QuadPart(const std::string& uniqueName = "QuadPart") : Instance(uniqueName, {}) {};
+	QuadPart() = default;
+	QuadPart(const QuadPart&) = default;
 
+	std::unique_ptr<Instance> clone() const override {
+		return std::make_unique<QuadPart>();
+	}
 private:
 
 };
