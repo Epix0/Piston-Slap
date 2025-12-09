254ca7b49c1fc2eaa2e5776b508b02d2d511aafe
Epix0 <isaacstettler@gmail.com>
2025-12-08 20:14:00 -0500

    rmv cmBaseName

--- Instance.h
+++ Instance.h
@@ -4,11 +4,16 @@
 #include "ShaderProgram.h"
 #include <string>
 #include <memory>
+#include <utility>
+#include "CustomIModelImporter.h"
 
 // An Instance is a world unit that is used in the game loop. An Instance
 // will have transformation data and mesh data.
 
 class Instance : public WorldObject {
+protected:
+	Instance() : mFriendlyName("Unnamed_Instance"), mpModel{} {};
+	Instance(std::weak_ptr<Model> pModel) : mFriendlyName("Unnamed_Instance"), mpModel(pModel) {};
 public:
 	virtual ~Instance() = default;
 
@@ -21,13 +26,23 @@ public:
 
 	virtual std::unique_ptr<Instance> clone() const = 0;
 
+	virtual inline void setFriendlyName(std::string newName) {
+		mFriendlyName = std::move(newName);
+	}
+
+	virtual inline std::string getFriendlyName() const {
+		return mFriendlyName;
+	}
+
 	inline std::shared_ptr<Model> getModel() const {
 		return mpModel.lock();
+	}
+
+	inline void setModel(std::weak_ptr<Model> pModel) {
+		mpModel = std::move(pModel);
 	}
 private:
 	// The reference points to a target element from CustomImporter.mImportedModels
 	std::weak_ptr<Model> mpModel;
-
-	// The technical alias of this Instance during registration. This is also the key to use when constructing.
-	const std::string cmBaseName;
+	std::string mFriendlyName;
 };
